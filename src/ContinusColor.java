b1c02b02ae98dafcb92693a0c30a111eb51a2e9c
dulaj <dulajm@orelit.com>
2022-06-29 00:59:38 +0530

    update

--- src/ContinusColor.java
+++ src/ContinusColor.java
@@ -1,85 +1,86 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public class ContinusColor {
 
+
+    static int[][] input = {
+            {1, 4, 4, 4, 4},
+            {2, 1, 1, 4, 3},
+            {3, 1, 1, 1, 2},
+            {3, 3, 2, 1, 1},
+            {1, 1, 3, 1, 1}};
+
+
+    static List<ValueXY> result = new ArrayList<ValueXY>();
+    static List<ValueXY> temp = new ArrayList<ValueXY>();
+
     public static void main(String args[]) {
 
-        int input[][] = {{1, 4, 4, 4, 4},
-                {2, 1, 1, 4, 3},
-                {3, 2, 1, 1, 2},
-                {3, 3, 2, 1, 2},
-                {1, 1, 3, 1, 1}};
+        for (int y = 0; y < 5; y++) {
 
-        int row=4;
-        int column=4;
-
-        colorGrid(input, row, column);
-
+            for (int x = 0; x < 5; x++) {
+                temp = new ArrayList<ValueXY>();
+                ValueXY coordinate = new ValueXY(x,y);
+                temp.add(coordinate);
+                checkAdjacent(x, y);
+                if (temp.size() > result.size()) {
+                    result = temp;
+                }
+            }
+        }
+        printGrid(result);
 
     }
 
-    private static void colorGrid(int[][] input, int row, int column) {
+    static  boolean f= false;
+    static void checkAdjacent(int x, int y) {
 
 
-        List<ValueXY> result = new ArrayList<ValueXY>();
-        List<ValueXY> temp = new ArrayList<ValueXY>();
+        ValueXY coordinate = new ValueXY();
 
-
-        for(int x=0; x<row; x++){
-
-            for(int y=0; y<column; y++){
-
-                temp = new ArrayList<ValueXY>();
-                ValueXY xy;
-
-                for(int j=x; j<row; j++){
-
-                    if(input[x][j] == input[x+1][j]){
-                         xy=new ValueXY(x+1,j);
-                        temp.add(xy);
-                       // input[x+1][j]=0;
-
-                    }
-                    if(input[x][j] == input[x][j+1]){
-
-                        xy=new ValueXY(x,j+1);
-                        temp.add(xy);
-                        //input[x][j+1]=0;
-                    }
-
-                    if(input[x][j] == input[x+1][j] || input[x][j] == input[x][j+1]){
-
-                        xy=new ValueXY(x,j);
-                        temp.add(xy);
-                        //input[x][j]=0;
-
-                    }
-                    else {
-                        break;
-                    }
-                }
-            }
-            if(temp.size()>result.size()){
-                result = new ArrayList<>();
-                result = temp;
-            }
+        if (x != 4 && input[x][y] == input[x + 1][y]) {
+            coordinate = new ValueXY(x+1,y);
+            temp.add(coordinate);
+            f=true;
+            checkAdjacent(x+1 , y);
+        }
+        if (y != 4 && input[x][y] == input[x][y + 1]) {
+            coordinate = new ValueXY(x,y+1);
+            temp.add(coordinate);
+            f=true;
+            checkAdjacent(x , y+1);
         }
 
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < column; j++) {
+//        if(!f){
+//            return;
+//        }
 
-                if(input[i][j]==0){
-                    System.out.print("*");
+        return;
+
+    }
+
+    private static void printGrid(List<ValueXY> coordinateList) {
+        ValueXY coordinate = new ValueXY();
+        Integer count=0;
+        for (int y = 0; y < 5; y++) {
+            for (int x = 0; x < 5; x++) {
+
+                coordinate = new ValueXY(y,x);
+
+                if (coordinateList.contains(coordinate)) {
+                    count++;
+                    System.out.print(" "+input[y][x]+" ");
                 }else{
-                    System.out.print(".");
+                    System.out.print(" . ");
                 }
-
             }
             System.out.println();
         }
-
-
-        System.out.println(result.size());
+        System.out.println("");
+        System.out.println("Continuous Color Grid Count: "+count);
     }
+
 }
--- src/ValueXY.java
+++ src/ValueXY.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class ValueXY {
 
     private int x;
@@ -14,5 +16,27 @@ public class ValueXY {
 
     public int getX() {
         return x;
+    }
+
+    public ValueXY() {
+    }
+
+    @Override
+    public String toString() {
+        return x + " " + y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ValueXY valueXY = (ValueXY) o;
+        return x == valueXY.x &&
+                y == valueXY.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
     }
 }
